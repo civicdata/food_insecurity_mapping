f0993d33599909918b057d24b792965138723912
rkahne <rkahne@gmail.com>
2018-02-17 15:36:13 -0500

    Fix it

---
+++ landline/data/20180116-1617-homicides-neighborhoods.js
@@ -0,0 +1,632 @@
+var neighborhood_homicides = {
+    "RUSSELL": {
+        "neighborhood": "RUSSELL", 
+        "area_calc": "3634150.28", 
+        "west": "1", 
+        "murder16": "10", 
+        "murder17": "10", 
+        "murders1617": "20", 
+        "diff_1617": "0"
+    }, 
+    "SHAWNEE": {
+        "neighborhood": "SHAWNEE", 
+        "area_calc": "5569461.44", 
+        "west": "1", 
+        "murder16": "9", 
+        "murder17": "7", 
+        "murders1617": "16", 
+        "diff_1617": "-2"
+    }, 
+    "PARKLAND": {
+        "neighborhood": "PARKLAND", 
+        "area_calc": "2106677.35", 
+        "west": "1", 
+        "murder16": "8", 
+        "murder17": "6", 
+        "murders1617": "14", 
+        "diff_1617": "-2"
+    }, 
+    "CHICKASAW": {
+        "neighborhood": "CHICKASAW", 
+        "area_calc": "3151851.8", 
+        "west": "1", 
+        "murder16": "4", 
+        "murder17": "7", 
+        "murders1617": "11", 
+        "diff_1617": "3"
+    }, 
+    "ALGONQUIN": {
+        "neighborhood": "ALGONQUIN", 
+        "area_calc": "3085921.05", 
+        "west": "1", 
+        "murder16": "3", 
+        "murder17": "7", 
+        "murders1617": "10", 
+        "diff_1617": "4"
+    }, 
+    "CALIFORNIA": {
+        "neighborhood": "CALIFORNIA", 
+        "area_calc": "3184540.13", 
+        "west": "1", 
+        "murder16": "3", 
+        "murder17": "6", 
+        "murders1617": "9", 
+        "diff_1617": "3"
+    }, 
+    "PORTLAND": {
+        "neighborhood": "PORTLAND", 
+        "area_calc": "6511854.79", 
+        "west": "1", 
+        "murder16": "4", 
+        "murder17": "4", 
+        "murders1617": "8", 
+        "diff_1617": "0"
+    }, 
+    "TAYLOR BERRY": {
+        "neighborhood": "TAYLOR BERRY", 
+        "area_calc": "2678020.8", 
+        "west": "1", 
+        "murder16": "4", 
+        "murder17": "4", 
+        "murders1617": "8", 
+        "diff_1617": "0"
+    }, 
+    "PARK HILL": {
+        "neighborhood": "PARK HILL", 
+        "area_calc": "2601011.21", 
+        "west": "1", 
+        "murder16": "4", 
+        "murder17": "3", 
+        "murders1617": "7", 
+        "diff_1617": "-1"
+    }, 
+    "SOUTHSIDE": {
+        "neighborhood": "SOUTHSIDE", 
+        "area_calc": "2381589.88", 
+        "west": "1", 
+        "murder16": "3", 
+        "murder17": "3", 
+        "murders1617": "6", 
+        "diff_1617": "0"
+    }, 
+    "BEECHMONT": {
+        "neighborhood": "BEECHMONT", 
+        "area_calc": "3741708.54", 
+        "west": "1", 
+        "murder16": "4", 
+        "murder17": "0", 
+        "murders1617": "4", 
+        "diff_1617": "-4"
+    }, 
+    "HAZELWOOD": {
+        "neighborhood": "HAZELWOOD", 
+        "area_calc": "1662732.12", 
+        "west": "1", 
+        "murder16": "3", 
+        "murder17": "1", 
+        "murders1617": "4", 
+        "diff_1617": "-2"
+    }, 
+    "PARK DUVALLE": {
+        "neighborhood": "PARK DUVALLE", 
+        "area_calc": "2356832.86", 
+        "west": "1", 
+        "murder16": "3", 
+        "murder17": "1", 
+        "murders1617": "4", 
+        "diff_1617": "-2"
+    }, 
+    "SOUTH LOUISVILLE": {
+        "neighborhood": "SOUTH LOUISVILLE", 
+        "area_calc": "2006513.24", 
+        "west": "1", 
+        "murder16": "4", 
+        "murder17": "0", 
+        "murders1617": "4", 
+        "diff_1617": "-4"
+    }, 
+    "JACOBS": {
+        "neighborhood": "JACOBS", 
+        "area_calc": "1823191.73", 
+        "west": "1", 
+        "murder16": "2", 
+        "murder17": "1", 
+        "murders1617": "3", 
+        "diff_1617": "-1"
+    }, 
+    "WYANDOTTE/OAKDALE": {
+        "neighborhood": "WYANDOTTE/OAKDALE", 
+        "area_calc": "1406404.52", 
+        "west": "1", 
+        "murder16": "2", 
+        "murder17": "1", 
+        "murders1617": "3", 
+        "diff_1617": "-1"
+    }, 
+    "CLOVERLEAF": {
+        "neighborhood": "CLOVERLEAF", 
+        "area_calc": "1877651.02", 
+        "west": "1", 
+        "murder16": "1", 
+        "murder17": "0", 
+        "murders1617": "1", 
+        "diff_1617": "-1"
+    }, 
+    "IROQUOIS": {
+        "neighborhood": "IROQUOIS", 
+        "area_calc": "610278.68", 
+        "west": "1", 
+        "murder16": "0", 
+        "murder17": "1", 
+        "murders1617": "1", 
+        "diff_1617": "1"
+    }, 
+    "AUBURNDALE": {
+        "neighborhood": "AUBURNDALE", 
+        "area_calc": "1585813.42", 
+        "west": "1", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "HALLMARK": {
+        "neighborhood": "HALLMARK", 
+        "area_calc": "357706.03", 
+        "west": "1", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "IROQUOIS PARK": {
+        "neighborhood": "IROQUOIS PARK", 
+        "area_calc": "3552336.77", 
+        "west": "1", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "KENWOOD HILL": {
+        "neighborhood": "KENWOOD HILL", 
+        "area_calc": "1339844.89", 
+        "west": "1", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "SOUTHLAND PARK": {
+        "neighborhood": "SOUTHLAND PARK", 
+        "area_calc": "1765709.17", 
+        "west": "1", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "WILDER PARK": {
+        "neighborhood": "WILDER PARK", 
+        "area_calc": "959209.48", 
+        "west": "1", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "SHELBY PARK": {
+        "neighborhood": "SHELBY PARK", 
+        "area_calc": "1052431.92", 
+        "west": "0", 
+        "murder16": "5", 
+        "murder17": "2", 
+        "murders1617": "7", 
+        "diff_1617": "-3"
+    }, 
+    "CENTRAL BUSINESS DISTRICT": {
+        "neighborhood": "CENTRAL BUSINESS DISTRICT", 
+        "area_calc": "3068323.51", 
+        "west": "0", 
+        "murder16": "2", 
+        "murder17": "2", 
+        "murders1617": "4", 
+        "diff_1617": "0"
+    }, 
+    "OLD LOUISVILLE": {
+        "neighborhood": "OLD LOUISVILLE", 
+        "area_calc": "3102253.98", 
+        "west": "0", 
+        "murder16": "2", 
+        "murder17": "1", 
+        "murders1617": "3", 
+        "diff_1617": "-1"
+    }, 
+    "PHOENIX HILL": {
+        "neighborhood": "PHOENIX HILL", 
+        "area_calc": "1510278.07", 
+        "west": "0", 
+        "murder16": "3", 
+        "murder17": "0", 
+        "murders1617": "3", 
+        "diff_1617": "-3"
+    }, 
+    "BASHFORD MANOR": {
+        "neighborhood": "BASHFORD MANOR", 
+        "area_calc": "1437456.43", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "2", 
+        "murders1617": "2", 
+        "diff_1617": "2"
+    }, 
+    "BON AIR": {
+        "neighborhood": "BON AIR", 
+        "area_calc": "3191812.21", 
+        "west": "0", 
+        "murder16": "1", 
+        "murder17": "1", 
+        "murders1617": "2", 
+        "diff_1617": "0"
+    }, 
+    "SAINT JOSEPH": {
+        "neighborhood": "SAINT JOSEPH", 
+        "area_calc": "1567194.83", 
+        "west": "0", 
+        "murder16": "2", 
+        "murder17": "0", 
+        "murders1617": "2", 
+        "diff_1617": "-2"
+    }, 
+    "BUTCHERTOWN": {
+        "neighborhood": "BUTCHERTOWN", 
+        "area_calc": "2378182.82", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "1", 
+        "murders1617": "1", 
+        "diff_1617": "1"
+    }, 
+    "CAMP TAYLOR": {
+        "neighborhood": "CAMP TAYLOR", 
+        "area_calc": "1079465.89", 
+        "west": "0", 
+        "murder16": "1", 
+        "murder17": "0", 
+        "murders1617": "1", 
+        "diff_1617": "-1"
+    }, 
+    "CHEROKEE TRIANGLE": {
+        "neighborhood": "CHEROKEE TRIANGLE", 
+        "area_calc": "2534944.93", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "1", 
+        "murders1617": "1", 
+        "diff_1617": "1"
+    }, 
+    "CLIFTON": {
+        "neighborhood": "CLIFTON", 
+        "area_calc": "1765934.77", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "1", 
+        "murders1617": "1", 
+        "diff_1617": "1"
+    }, 
+    "LIMERICK": {
+        "neighborhood": "LIMERICK", 
+        "area_calc": "587723.84", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "1", 
+        "murders1617": "1", 
+        "diff_1617": "1"
+    }, 
+    "SCHNITZELBURG": {
+        "neighborhood": "SCHNITZELBURG", 
+        "area_calc": "1500979.46", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "1", 
+        "murders1617": "1", 
+        "diff_1617": "1"
+    }, 
+    "SMOKETOWN JACKSON": {
+        "neighborhood": "SMOKETOWN JACKSON", 
+        "area_calc": "1025096.43", 
+        "west": "0", 
+        "murder16": "1", 
+        "murder17": "0", 
+        "murders1617": "1", 
+        "diff_1617": "-1"
+    }, 
+    "AUDUBON": {
+        "neighborhood": "AUDUBON", 
+        "area_calc": "1609600.1", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "AVONDALE MELBOURNE HEIGHTS": {
+        "neighborhood": "AVONDALE MELBOURNE HEIGHTS", 
+        "area_calc": "1254820.22", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "BELKNAP": {
+        "neighborhood": "BELKNAP", 
+        "area_calc": "2046354.28", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "BONNYCASTLE": {
+        "neighborhood": "BONNYCASTLE", 
+        "area_calc": "844571.08", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "BOWMAN": {
+        "neighborhood": "BOWMAN", 
+        "area_calc": "3281879.27", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "BROWNSBORO ZORN": {
+        "neighborhood": "BROWNSBORO ZORN", 
+        "area_calc": "2041604.89", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "CHEROKEE GARDENS": {
+        "neighborhood": "CHEROKEE GARDENS", 
+        "area_calc": "1014958.03", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "CHEROKEE SENECA": {
+        "neighborhood": "CHEROKEE SENECA", 
+        "area_calc": "3410245.82", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "CLIFTON HEIGHTS": {
+        "neighborhood": "CLIFTON HEIGHTS", 
+        "area_calc": "1658993.8", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "CRESCENT HILL": {
+        "neighborhood": "CRESCENT HILL", 
+        "area_calc": "4923528.19", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "DEER PARK": {
+        "neighborhood": "DEER PARK", 
+        "area_calc": "1272069.81", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "EDGEWOOD": {
+        "neighborhood": "EDGEWOOD", 
+        "area_calc": "1927346.27", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "FAIRGROUNDS": {
+        "neighborhood": "FAIRGROUNDS", 
+        "area_calc": "2802605.43", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "GARDINER LANE": {
+        "neighborhood": "GARDINER LANE", 
+        "area_calc": "768933.38", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "GERMANTOWN": {
+        "neighborhood": "GERMANTOWN", 
+        "area_calc": "1552880.71", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "HAWTHORNE": {
+        "neighborhood": "HAWTHORNE", 
+        "area_calc": "1137485.33", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "HAYFIELD DUNDEE": {
+        "neighborhood": "HAYFIELD DUNDEE", 
+        "area_calc": "1526785.14", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "HIGHLAND PARK": {
+        "neighborhood": "HIGHLAND PARK", 
+        "area_calc": "1518331.89", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "HIGHLANDS": {
+        "neighborhood": "HIGHLANDS", 
+        "area_calc": "472275.66", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "HIGHLANDS DOUGLASS": {
+        "neighborhood": "HIGHLANDS DOUGLASS", 
+        "area_calc": "1668774.2", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "HIKES POINT": {
+        "neighborhood": "HIKES POINT", 
+        "area_calc": "2318206.4", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "IRISH HILL": {
+        "neighborhood": "IRISH HILL", 
+        "area_calc": "1034903.26", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "KLONDIKE": {
+        "neighborhood": "KLONDIKE", 
+        "area_calc": "2119885.13", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "MERRIWETHER": {
+        "neighborhood": "MERRIWETHER", 
+        "area_calc": "673421.3", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "PARISTOWN POINTE": {
+        "neighborhood": "PARISTOWN POINTE", 
+        "area_calc": "175223.09", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "POPLAR LEVEL": {
+        "neighborhood": "POPLAR LEVEL", 
+        "area_calc": "3140655.22", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "PRESTONIA": {
+        "neighborhood": "PRESTONIA", 
+        "area_calc": "965676.16", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "REMAINDER OF CITY": {
+        "neighborhood": "REMAINDER OF CITY", 
+        "area_calc": "2376593.52", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "ROCKCREEK LEXINGTON ROAD": {
+        "neighborhood": "ROCKCREEK LEXINGTON ROAD", 
+        "area_calc": "1551545.13", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "STANDIFORD": {
+        "neighborhood": "STANDIFORD", 
+        "area_calc": "707198.25", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "TYLER PARK": {
+        "neighborhood": "TYLER PARK", 
+        "area_calc": "1332299.44", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }, 
+    "UNIVERSITY": {
+        "neighborhood": "UNIVERSITY", 
+        "area_calc": "2114220.05", 
+        "west": "0", 
+        "murder16": "0", 
+        "murder17": "0", 
+        "murders1617": "0", 
+        "diff_1617": "0"
+    }
+}
